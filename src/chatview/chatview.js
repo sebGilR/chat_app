74f8579fb0c522a6270c6d86f913dc2105f2f08c
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-06-27 11:18:25 -0500

    Add autoscroll to last message in chat

--- src/chatview/chatview.js
+++ src/chatview/chatview.js
@@ -3,17 +3,26 @@ import style from './style';
 import { withStyles } from '@material-ui/core/styles';
 
 class ChatViewComponent extends React.Component {
+  componentDidUpdate = () => {
+    const container = document.getElementById('chatview-container');
+
+    if (container) {
+      container.scrollTo(0, container.scrollHeight)
+    }
+  }
 
   render() {
     const { classes, chat, user } = this.props
 
     if (chat === undefined) {
-      return (<main className={classes.content}></main>);
+      return (<main id='chatview-container' className={classes.content}></main>);
     } else {
       return (
         <div>
-          <div></div>
-          <main className={classes.content}>
+          <div className={classes.chatHeader}>
+            Chat with {chat.users.filter(friend => friend !== user)[0]}
+          </div>
+          <main id='chatview-container' className={classes.content}>
             {
               chat.messages.map((msg, index) => {
                 return (
