3a276ee08af95bec96c798582b120186ed5c6bfc
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-06-26 22:48:17 -0500

    Initial project setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+
+ReactDOM.render(
+  <div>Hi there!</div>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
